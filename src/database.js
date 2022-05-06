5d93a8449d62d5892cc0c22093a56c62d5eed63d
Carlos Avalos <cipotex@hotmail.com>
2022-05-06 11:15:38 -0600

    se cambio conexion a bd para usar mongodb en la nube

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/angular-auth', {
+mongoose.connect('mongodb+srv://admin:admin@cluster0.uho69.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
